04bc9e662c1fadf478c6afa8522a0273673713df
Syntax <63171334+cannabispowered@users.noreply.github.com>
2021-03-03 23:49:08 -0800

    Create dump-pending.py

---
+++ dump-pending.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+token = input("Token: ")
+
+header_payload = {
+    'Authorization' : token,
+    'User-Agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36',
+}
+
+r = requests.get('https://discord.com/')
+cookie_payload = {
+    '__cfduid' : r.cookies['__cfduid'],
+}
+
+r = requests.get('https://discordapp.com/api/v6/users/@me/relationships',cookies=cookie_payload,headers=header_payload)
+
+response_data = json.loads(r.text)
+
+for i in response_data:
+    if i['type'] == 4:
+        try:
+            print(i['user']['username']+"#"+i['user']['discriminator']) 
+        except:
+            pass
